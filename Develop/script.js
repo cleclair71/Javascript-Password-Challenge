d113f504a2b1ba6b2d15354fa963afeae598bf10
cass <76407605+cleclair71@users.noreply.github.com>
2023-01-07 16:24:40 -0500

    working on script functions

--- Develop/script.js
+++ Develop/script.js
@@ -5,8 +5,11 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(length - 12) {
+  var chars= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*0123456789";
+  var passwordLength = length;
   var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
